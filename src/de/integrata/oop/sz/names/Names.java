41b365b28f0df510311f458c701a33ca2aa22148
Stephan Pauxberger <stephan@blackbuild.com>
2015-11-24 08:48:52 +0100

    Split into abstraction methods

--- src/de/integrata/oop/sz/names/Names.java
+++ src/de/integrata/oop/sz/names/Names.java
@@ -3,18 +3,23 @@ package de.integrata.oop.sz.names;
 public class Names {
 
 	public void printPrimesBelow(int upperLimit) {
-		for (int candidate = 1; candidate < upperLimit; candidate++) { 
-		    boolean isPrime = true; 
-		    for (int divider = 2; divider < candidate; divider++) { 
-		        if (candidate % divider == 0) { 
-		            isPrime = false; 
-		            break; 
-		        } 
-		    } 
-		    if (isPrime) { 
-		        System.out.println(candidate); 
-		    } 
-		} 
+		for (int candidate = 1; candidate < upperLimit; candidate++)
+			printIfPrime(candidate); 
+	}
+
+	private void printIfPrime(int candidate) {
+		if (isPrime(candidate))
+			System.out.println(candidate);
+	}
+
+	private boolean isPrime(int candidate) {
+		for (int divider = 2; divider < candidate; divider++)
+			if (canBeDivided(candidate, divider)) return false;
+		return true;
+	}
+
+	private boolean canBeDivided(int candidate, int divider) {
+		return candidate % divider == 0;
 	}
 	
 }
