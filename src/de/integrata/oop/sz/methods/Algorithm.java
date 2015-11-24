8169ff60701578ade29b64723f4a12d1f8bc5548
Stephan Pauxberger <stephan@blackbuild.com>
2015-11-24 08:49:57 +0100

    High level methods

---
+++ src/de/integrata/oop/sz/methods/Algorithm.java
@@ -0,0 +1,31 @@
+package de.integrata.oop.sz.methods;
+
+public class Algorithm {
+
+	public void printOrderConfirmation() {
+		printAddress();
+		printSalutation();
+		printShoppingBasket();
+		printRebate();
+		printTotal();
+	}
+	
+	public int calculateRebate() {
+		int result
+		if (customer.isVip())
+			return 10;
+		if (orderValue > 100) {
+			result = 5
+		}
+		if (basket.containsSpecialItems()) 
+			result += basket.specialRebate();
+			
+					
+			
+			
+		
+		
+		
+	}
+	
+}
